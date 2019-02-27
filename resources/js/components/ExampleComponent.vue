a02bd7252d651dd68dd34f62afeca357e62f8362
Chris Mewton <chris@cardealer.co.uk>
2019-02-27 15:58:36 +0000

    Vue JS components

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,52 +1,48 @@
 <template>
-    <div class="container" style="margin-top:50px;">
+    <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header"><strong> Car Dealer Vue JS Infinite Scroll</strong></div>
- 
-                    <div class="card-body">
-                        <div>
-                          <p v-for="item in list">
-                            <em>{{item.make}}</em> <a v-bind:href="'https://www.cardealer.co.uk/vehicle/'+item.slug" target="_blank">{{item.model}}</a>
-                          </p>
-                          <infinite-loading @distance="1" @infinite="infiniteHandler"></infinite-loading>
- 
-                        </div>
-                    </div>
-                </div>
+            <div class="col-md-8">
+                 <center><h2>Vue Laravel Bootstrap Owl Carousel Working Example:</h2></center>  
+         <div id="demo" class="carousel slide" data-ride="carousel">
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+          </ol>
+
+          <!-- Wrapper for slides -->
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="https://responsivedesign.is/wp-content/uploads/2016/11/Owl-Carousel-2_ri92wj.jpg" alt="Los Angeles" style="width:100%;">
+            </div>
+
+            <div class="carousel-item">
+            <img src="https://responsivedesign.is/wp-content/uploads/2016/11/Owl-Carousel-2_ri92wj.jpg" alt="Chicago" style="width:100%;">
+            </div>
+          
+            <div class="carousel-item">
+            <img src="https://responsivedesign.is/wp-content/uploads/2016/11/Owl-Carousel-2_ri92wj.jpg" alt="New york" style="width:100%;">
+            </div>
+          </div>
+
+          <!-- Left and right controls -->
+            <a class="carousel-control-prev" href="#demo" data-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+            </a>
+            <a class="carousel-control-next" href="#demo" data-slide="next">
+            <span class="carousel-control-next-icon"></span>
+            </a>
+          </div>
             </div>
         </div>
     </div>
 </template>
- 
+
 <script>
     export default {
         mounted() {
             console.log('Component mounted.')
-        },
-        data() {
-            return {
-              list: [],
-              page: 1,
-            };
-          },
-          methods: {
-            infiniteHandler($state) {
-                let vm = this;
- 
-                this.$http.get('/car?page='+this.page)
-                    .then(response => {
-                        return response.json();
-                    }).then(data => {
-                        $.each(data.data, function(key, value) {
-                                vm.list.push(value);
-                        });
-                        $state.loaded();
-                    });
- 
-                this.page = this.page + 1;
-            },
-          },
+        }
     }
 </script>
---
+++ resources/js/components/Flash.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="alert alert-success" role="alert">
+        <strong>Success!</strong> {{ body }}
+    </div>
+</template>
+ 
+<script>
+    export default {
+        props: ['message'],
+ 
+        data() {
+            return {
+                body: this.message
+            }
+        }
+    }
+</script>
